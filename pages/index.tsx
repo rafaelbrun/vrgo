fa9b3ecacfe950c946d3b0d76b536c9d65195dc5
rafaelbrun <rafaelbrungolin@gmail.com>
2020-12-01 20:19:57 -0400

    Formatacao e responsividade

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
         <title>VRGO - Realidade Virtual</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <header className={styles.header}>
         <div className={styles.header_nav_first}>
@@ -18,17 +19,17 @@ export default function Home() {
           </a>
         </div>
         <div className={styles.header_nav_second}>
-          <a href="#" className={tooltip.tooltip}>
+          <a href="#produto" className={tooltip.tooltip}>
             <h4>Produto</h4>
             <span className={tooltip.tooltiptext}>O que vendemos, tudo sobre nosso produto.</span>
           </a>
           <h4>|</h4>
-          <a href="#" className={tooltip.tooltip}>
+          <a href="#vrgo" className={tooltip.tooltip}>
             <h4>VRGO</h4>
             <span className={tooltip.tooltiptext}>Tudo sobre a nossa empresa.</span>
           </a>
           <h4>|</h4>
-          <a href="#" className={tooltip.tooltip}>
+          <a href="#contato" className={tooltip.tooltip}>
             <h4>Contato</h4>
             <span className={tooltip.tooltiptext}>Onde nos encontrar.</span>
           </a>
@@ -49,13 +50,19 @@ export default function Home() {
           <div className={styles.divisor_dot} />
           <div className={styles.divisor_line} />
         </div>
-        <iframe className={styles.video_div} src="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-        <div className={styles.div_divisor} >
-          <div className={styles.divisor_line} />
-          <div className={styles.divisor_dot} />
-          <div className={styles.divisor_line} />
+        <iframe className={styles.video_div} src="https://www.youtube.com/embed/tgbNymZ7vqY" />
+        <div className={styles.div_produto} id="produto">
+          <h1>Produto</h1>
+          <p>Tudo sobre o que fazemos</p>
         </div>
-
+        <div className={styles.div_vrgo} id="vrgo">
+          <h1>VRGO</h1>
+          <p>Tudo sobre nossa empresa</p>
+        </div>
+        <div className={styles.div_contato} id="contato">
+          <h1>CONTATO</h1>
+          <p>Como nos contatar</p>
+        </div>
       </main>
     </div>
   )
