20c1e0ed4bc5df9e799cd53ea812b261f5ab69e0
rafaelbrun <rafaelbrungolin@gmail.com>
2020-12-02 20:11:22 -0400

    footer work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { ReactElement } from 'react'
 
-function MyApp({ Component, pageProps }) {
+library.add(fab, far)
+
+function MyApp({ Component, pageProps }): ReactElement {
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import tooltip from '../styles/Tooltip.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/6c15b25352.js"></script>
         <title>VRGO - Realidade Virtual</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -35,8 +38,9 @@ export default function Home() {
           </a>
         </div>
         <div className={styles.header_nav_third}>
-          <a href="#">
-            <h4>SOCIAL?</h4>
+        <a href="#contato" className={tooltip.tooltip}>
+        <FontAwesomeIcon icon={["far", "question-circle"]} className={styles.icons} />
+            <span className={tooltip.tooltiptext}>Tem alguma dúvida?</span>
           </a>
         </div>
       </header>
@@ -51,17 +55,51 @@ export default function Home() {
           <div className={styles.divisor_line} />
         </div>
         <iframe className={styles.video_div} src="https://www.youtube.com/embed/tgbNymZ7vqY" />
-        <div className={styles.div_produto} id="produto">
-          <h1>Produto</h1>
-          <p>Tudo sobre o que fazemos</p>
+        <div className={styles.div_divs_infos}>
+          <div className={styles.div_produto} id="produto">
+            <h1>PRODUTO</h1>
+            <p>Tudo sobre o que fazemos</p>
+          </div>
+          <div className={styles.div_vrgo} id="vrgo">
+            <h1>VRGO</h1>
+            <p>Tudo sobre nossa empresa</p>
+          </div>
+          <div className={styles.div_contato} id="contato">
+            <h1>CONTATO</h1>
+            <p>Como nos contatar</p>
+          </div>
         </div>
-        <div className={styles.div_vrgo} id="vrgo">
-          <h1>VRGO</h1>
-          <p>Tudo sobre nossa empresa</p>
+
+        <div className={styles.footer}>
+          <div className={styles.footer_first}>
+            <a href="#">LOGO</a>
+          </div>
+          <div className={styles.footer_sec}>
+            <p>SOCIAL</p>
+            <a href="https://www.instagram.com/vrgo.rvirtual/" target="_blank">
+              <FontAwesomeIcon icon={["fab", "instagram"]} className={styles.icons} />
+            </a>
+          </div>
+          <div className={styles.footer_thirdy}>
+            <p>CONTATO</p>
+            <div>
+              <div className={styles.div_contato_icon}>
+                <FontAwesomeIcon icon={["fab", "whatsapp"]} className={styles.icons} />
+                +5565999208836
+              </div>
+              <div className={styles.div_contato_icon}>
+                <FontAwesomeIcon icon={["far", "envelope"]} className={styles.icons} />
+                vrgo@gmail.com
+              </div>
+
+            </div>
+          </div>
+
         </div>
-        <div className={styles.div_contato} id="contato">
-          <h1>CONTATO</h1>
-          <p>Como nos contatar</p>
+
+        <div className={styles.footer_footer}>
+          <p>VRGO Realidade Virtual 2020 ©</p>
+          <p>Cuiabá - Mato Grosso</p>
         </div>
       </main>
     </div>
